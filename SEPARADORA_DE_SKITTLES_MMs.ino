7a8a4c67a5f5934561fa67e99313238b016f2d6e
Raphael F. Oliveira <ledrix@hotmail.com>
2024-10-31 13:56:36 -0300

    Funcionando razoavel

--- SEPARADORA_DE_SKITTLES_MMs.ino
+++ SEPARADORA_DE_SKITTLES_MMs.ino
@@ -7,18 +7,21 @@ Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS3472
 
 Servo wheelServo;  // create a servo object to control a servo
 Servo chuteServo;  // create another servo object to control a servo
-Servo mixerServo;
+Servo mixerServo;  // crete another servo object to control servo
 
-const byte LED_DIGITAL = { 10 };
+const byte BOTAO_START_STOP = { 2 };
+const byte LED_DIGITAL = { 3 };
+const byte SENSOR_RODA = { 8 };
+const byte SENSOR_TUBO = { 9 };
+const byte VIBRA = { 10 };
+
 const byte NUM_LEDS =  5; // QUANTIDADE DE LEDS ENDERECAVEIS
 CRGB leds[NUM_LEDS];
 
-const int calibrating = 0; //change this to 0 when you want to run the machine and to 1 when you set it up for the first time
-const int wheelOverRun = 80; //decrease this if the skittle goes past the colour sensor or increase it if the skittle stops before it reaches the sensor
-//270 para 90 --- 160 para 89 --- 110 para 88 -- 80 para 87
-const int infraRedSensor = 8;
 
-int switchState;
+const int wheelOverRun = 80; //decrease this if the skittle goes past the colour sensor or increase it if the skittle stops before it reaches the sensor
+//270 para 90 --- 165 para 89 --- 110 para 88 -- 80 para 87
+
 int arrayIndexToAimAt;
 
 float redReading, greenReading, blueReading; //somewhere to store our colour sensor readings
@@ -26,29 +29,35 @@ float redReading, greenReading, blueReading; //somewhere to store our colour sen
 const int arrayRows = 7;
 const int arrayColumns = 6;
 int SAMPLES[arrayRows][arrayColumns] = {  // array goes red value, green value, blue value, colour, difference to sample (update by code later), angle for depositor
-  {95, 82,  58,  "Red", 0, 30}, //red -0
-  {107,  75,  52,  "Orange", 0, 45}, //orange -1
-  {99,  87,  47,  "Yellow", 0, 60}, //yellow -2
-  {80,  94,  58,  "Green", 0, 90}, //green -3 
-  {73,  87,  72,  "Blue", 0, 105}, //blue -4   
-  {86,  86,  61, "Brown", 0, 120},  //brown -5
-  {91, 85, 59, "No Skittle", 0, 75}, //no skittle -6
+  {160, 64,  47,  "Red", 0, 20}, //red -- ok
+  {143,  55,  30,  "Orange", 0, 50}, //orange -1 -- ok
+  {118,  91,  28,  "Yellow", 0, 80}, //yellow -2 -- ok  
+  {68,  121,  44,  "Green", 0, 110}, //green -3 -- ok
+  {40,  87,  121,  "Blue", 0, 140}, //blue -4 
+  {100,  82,  54, "Brown", 0, 170},  //brown -5  
+  {86, 87, 59, "No Skittle", 0, 95}, //no skittle -6
 }; 
 // Determines the number of samples stored in the array
 const byte samplesCount = sizeof(SAMPLES) / sizeof(SAMPLES[0]);
 
+volatile bool toogle_start_stop = false; 
 
 void setup()
 {
-  pinMode(infraRedSensor, INPUT);
-  
-  Serial.begin(9600);
+  pinMode(SENSOR_RODA, INPUT);
+  pinMode(SENSOR_TUBO, INPUT);
+  pinMode(BOTAO_START_STOP, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), tootgle, FALLING);
+  pinMode(VIBRA, OUTPUT);
 
+  chuteServo.attach(5); // attaches the servo on pin d5 to the servo object
+  chuteServo.write(75); // No Sittle default position 
+  wheelServo.attach(6); // attaches the servo on pin d6 to the servo object
+  wheelServo.write(92); // stop motor
+  mixerServo.attach(7); // attaches the servo on pin d6 to the servo object
+  mixerServo.write(91); // stop motor
+ 
   
-  chuteServo.attach(5);  // attaches the servo on pin d5 to the servo object
-  wheelServo.attach(6);  // attaches the servo on pin d6 to the servo object
-  mixerServo.attach(7);  // attaches the servo on pin d7 to the servo object
-
     if (tcs.begin()) {
     //Serial.println("Found sensor");
   } else {
@@ -65,57 +74,100 @@ void setup()
 
 
 
-void loop()
-{
-  switchState = digitalRead(infraRedSensor);
+
+
+void loop() {  
+
+  if (toogle_start_stop == true) {
+
+    chuteServo.attach(5); // attaches the servo on pin d5 to the servo object     
+    wheelServo.attach(6); // attaches the servo on pin d6 to the servo object    
+    mixerServo.attach(7); // attaches the servo on pin d6 to the servo object
+    
+
+    if (digitalRead(SENSOR_RODA) == HIGH) {
+     wheelServo.write(87);
+    }
   
-  if (switchState == HIGH){
-    // if (calibrating == 0){
-    //   clearScreen();
-    // }
-    // Serial.println();
-    // Serial.println("Yes - contact switch aligned. Taking a reading....");
-    delay(wheelOverRun); //allows the wheel to keep turning - adjust this value to ensure your skittle stops beenath the colour sensor.
-    wheelServo.write(92);  //stops the skittle wheel    
-    readColour();
-    if (calibrating == 0){
-      matchColour();
+    static unsigned long debounce;
+    if ((millis() - debounce) > 10000) {    
+      while (digitalRead(SENSOR_TUBO) == LOW) { 
+        wheelServo.write(92);
+        mixerServo.write(97);
+        delay(500);
+        byte count = 0;     
+        while (count < 10) {
+        mixerServo.write(87);
+        digitalWrite(VIBRA, HIGH);
+        delay(250);
+        mixerServo.write(91);
+        digitalWrite(VIBRA, LOW);
+        delay(250);
+        count++; 
+        }           
+      }     
+      debounce = millis();                        
+    }          
+  
+    if (digitalRead(SENSOR_RODA) == HIGH){
+
+      delay(wheelOverRun); //allows the wheel to keep turning - adjust this value to ensure your skittle stops beenath the colour sensor.   
+      wheelServo.write(92);  //stops the skittle wheel
+      digitalWrite(VIBRA, LOW);    
+      readColour();    
+      matchColour();  
+      arrayIndexToAimAt = findClosestSample();
+      aimChute();  
+
+      delay(300); //gives the chute time to settle in place before we send the skittle down
+
+      } else {
+      //Serial.println("Skittle moving wheel not aligned wiht contact switch yet - please gently rotate by hand unless a reading is produced shortly");                 
+      wheelServo.write(87);  //starts to turn the skittle collecting wheel
+      digitalWrite(VIBRA, HIGH);
+    
     }
-    // if (calibrating == 0){
-    //   printArray(SAMPLES);
-    // }
-    arrayIndexToAimAt = findClosestSample();
-    aimChute();    
-    if (calibrating == 1){
-      waitForUser(); 
-    }
-    delay(500); //gives the chute time to settle in place before we send the skittle down
-    } else {
-    //Serial.println("Skittle moving wheel not aligned wiht contact switch yet - please gently rotate by hand unless a reading is produced shortly");     
-    wheelServo.write(87);  //starts to turn the skittle collecting wheel
-    //mixerServo.write(80);
+  } else {
+
+    wheelServo.detach();
+    mixerServo.detach();
+    chuteServo.detach();
+
+      for(int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB(255, 255, 0);              
+      }
+      FastLED.show();
+      delay(300);
+      for(int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB(0, 0, 0);        
+      }
+      FastLED.show();
+      delay(300);
   }
+     
+  
 }
 
 
 
+
+
+
+void tootgle() {
+   if(digitalRead(BOTAO_START_STOP) == LOW) {
+    toogle_start_stop = !toogle_start_stop;
+  }
+}
+
 void readColour()
-  {
-    delay(70);  // takes 50ms to read
-  
-    tcs.getRGB(&redReading, &greenReading, &blueReading);
+  { 
+    delay(300);  // takes 50ms to read
+    tcs.getRGB(&redReading, &greenReading, &blueReading);   
     
-    // Serial.println("Colour sensor currently reads:  ");
-    // Serial.print("R:"); Serial.print(int(redReading)); 
-    // Serial.print("\tG:"); Serial.print(int(greenReading)); 
-    // Serial.print("\tB:"); Serial.println(int(blueReading));
-    // Serial.println();
   }
 
-void matchColour()
-{
+void matchColour() {
   int colourDistance;
-
   // Iterate through the array to find a matching colour sample
   for (byte i = 0; i < samplesCount; i++)
   {
@@ -124,68 +176,38 @@ void matchColour()
   }   
 }
 
+int getColourVariance(int redSensor, int greenSensor, int blueSensor, int redSample, int greenSample, int blueSample, char *colour) {  
 
-
-int getColourVariance(int redSensor, int greenSensor, int blueSensor, int redSample, int greenSample, int blueSample, char *colour)
-{
-  // Serial.print("Checking the skittle against the ");
-  // Serial.print(colour);
-  // Serial.println(" sample.");
   int redDifference;
   redDifference = compareValues(redSensor, redSample, "Red ");
   int greenDifference;
   greenDifference = compareValues(greenSensor, greenSample, "Green ");
   int blueDifference;
-  blueDifference = compareValues(blueSensor, blueSample, "Blue ");
-  // Serial.print("Total variance between the measured colour and sample for the ");
-  // Serial.print(colour);
-  // Serial.print(" item in the array = ");
+  blueDifference = compareValues(blueSensor, blueSample, "Blue ");  
   int totalDifference;
-  totalDifference = redDifference + greenDifference + blueDifference;
-  // Serial.println(totalDifference);
-  // Serial.println();
+  totalDifference = redDifference + greenDifference + blueDifference;  
   return totalDifference;
-  
+
 }
 
-int compareValues(int sensorColour, int sampleColour, char *colour)
-{
-  int difference;
-  // Serial.print("Comparing Colours:");
-  // Serial.print(colour);
-  // Serial.print(int(sensorColour)); 
-  // Serial.print(" was measured compared with ");
-  // Serial.print(int(sampleColour));
-  // Serial.print(" which has a difference of = ");
+int compareValues(int sensorColour, int sampleColour, char *colour) {
+
+  int difference;  
   difference = sensorColour - sampleColour;
   if (difference < 0)
   {
     difference = difference * (-1);
-  }
-  // Serial.println(int(difference));
+  }  
   return difference;
+
 }
 
-// void printArray( const int a[][ arrayColumns ] ) {
-//    // loop through array's rows
-//    for ( int i = 0; i < arrayRows; ++i ) {
-//       // loop through columns of current row
-//       for ( int j = 0; j < arrayColumns; ++j ){
-//         Serial.print (a[ i ][ j ] );
-//         Serial.print(", ");
-//       }
-//       Serial.println();
-//    }
-//    Serial.println(); 
-// }
+int findClosestSample() {
 
-int findClosestSample()
-{
-  int index = 0 ;           
-
+  int index = 0;
   for (int i = 0; i < arrayRows; i++) {    
     if (SAMPLES[i][4] < SAMPLES[index][4]) {
-      index = i;
+      index = i;const byte LED_DIGITAL = { 11 };
     }
   }    
   switch (index) {
@@ -199,7 +221,7 @@ int findClosestSample()
     case 1:
       Serial.println("Laranja");
       for(int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CRGB(255, 100, 0);              
+        leds[i] = CRGB(255, 70, 0);              
       }
       FastLED.show();
       break;
@@ -227,9 +249,9 @@ int findClosestSample()
     case 5:
       Serial.println("Marron");
       for(int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CRGB(50, 10, 15);              
+        leds[i] = CRGB(255, 0, 150); 
+        FastLED.show();
       }
-      FastLED.show();
       break;
     default:
       //Serial.println("Branco");
@@ -240,29 +262,9 @@ int findClosestSample()
       break; 
   }  
   return index;
+
 }
 
-void aimChute()
-{
+void aimChute() {
   chuteServo.write(SAMPLES[arrayIndexToAimAt][5]);  
-}
-
-// void clearScreen() {
-//   for (int i = 0; i <= 80; i++) {
-//     Serial.println();
-//     //delay(10);
-//   }
-// }
-
-void waitForUser()
-{
-  Serial.println();
-  Serial.println("-----========   Waiting for a single character to be sent before proceeding     ======------");
-  while(Serial.available() == 0)
-  {
-    delay(1);
-  }
-  Serial.read();
-  Serial.read();
-  Serial.read();
 }
